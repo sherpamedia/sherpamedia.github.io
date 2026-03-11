4dfd2a99efb87232d38b29f61b35b9d917fd1479
boyanq <boyan@exceptcreate.com>
2026-03-11 11:14:53 +0200

    fix for deploy actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://sherpamedia.net",
+  site: "https://sherpamedia.github.io",
   integrations: [mdx(), sitemap()],
   // Enable View Transitions for SPA-like navigation (no full page reloads)
   experimental: {},
